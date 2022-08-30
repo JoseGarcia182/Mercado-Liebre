4d4e17468a5b5467a6f7bce0a6080d36447bd478
JoseGarcia182 <josepgar01@gmail.com>
2022-08-29 21:11:18 -0300

    configuracion carpeta static

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const app = express();
 
 app.listen(process.env.PORT || 3030, () => console.log("servidor andando"));
 
-const publicPath = path.resolve (__dirname, './public');
-app.use( express.static(publicPath) );
 
-app.get(express.static(path.join(__dirname, "/Public")));
+app.use( express.static("Public"));
+
+
 
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '/views/home.html'))
