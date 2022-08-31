7ffb5ece25720366d5958a29931ac298783d776c
JoseGarcia182 <josepgar01@gmail.com>
2022-08-31 19:23:21 -0300

    agregando register y logib

--- app.js
+++ app.js
@@ -12,3 +12,25 @@ app.use( express.static("public"));
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '/views/home.html'))
 });
+
+app.get('/register', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.post("/login", (req, res) => {
+    console.log("Acceso consedido");
+    res.redirect("/");
+});
+
+app.post("/register", (req, res) => {
+    console.log("Registro completado");
+    res.redirect("/");
+});
+
+app.get('/home', (req, res) =>{
+    res.sendFile(path.join(__dirname, '/views/home.html'))
+});
